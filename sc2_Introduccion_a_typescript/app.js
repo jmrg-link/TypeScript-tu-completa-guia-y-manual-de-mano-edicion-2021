3a32a5f0cf23eb5a7a88fcd1ddc6398b6a4cbbc4
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-08-16 21:30:32 +0200

    Add proyecto sc2: del ./ readme.md + ejemplo app.ts

--- sc2_Introduccion_a_typescript/app.js
+++ sc2_Introduccion_a_typescript/app.js
@@ -1,1 +1,8 @@
+"use strict";
 var msg = "hola mundo";
+console.log(msg);
+var hero = {
+    name: 'Ironman',
+    age: 45
+};
+console.log(hero.age);
--- sc2_Introduccion_a_typescript/app.ts
+++ sc2_Introduccion_a_typescript/app.ts
@@ -1,2 +1,10 @@
 let msg:string = "hola mundo"
 console.log(msg);
+
+
+const hero = {
+    name:'Ironman',
+    age: 45
+}
+
+console.log(hero.age);
