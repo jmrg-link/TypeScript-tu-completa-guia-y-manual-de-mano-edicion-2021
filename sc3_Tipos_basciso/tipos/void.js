dc754b2ed97f6ab804d329ea43ad7a6b084c7a1e
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-08-17 23:30:00 +0200

    añadiendo ficheros ejemplo void

---
+++ sc3_Tipos_basciso/tipos/void.js
@@ -0,0 +1,10 @@
+"use strict";
+(function () {
+    // funcion con void  no devuelve nada
+    function callBatman() {
+    }
+    var superman = function () {
+    };
+    var a = callBatman();
+    console.log(a);
+})();
---
+++ sc3_Tipos_basciso/tipos/void.ts
@@ -0,0 +1,14 @@
+(()=>{
+    // funcion con void  no devuelve nada
+    function callBatman():void{
+    
+    }
+
+    const superman = ():void=>{
+
+    }
+
+    const a = callBatman()
+    console.log(a);
+    
+})()
