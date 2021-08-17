3083dbea977be2457a35c2e74127185fb55696b8
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-08-17 22:53:43 +0200

    añadiendo ficheros boolean , any , number y strings

--- sc3_Tipos_basciso/tipos/any.js
+++ sc3_Tipos_basciso/tipos/any.js
@@ -1,3 +1,14 @@
 "use strict";
 (function () {
+    var avenger = 123;
+    var existe;
+    var power;
+    avenger = 'Dr strange';
+    //console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0));
+    avenger = 150.23256415;
+    //console.log(avenger.toFixed(2));
+    console.log(avenger.toFixed(2));
+    console.log(existe);
+    console.log(power);
 })();
--- sc3_Tipos_basciso/tipos/any.ts
+++ sc3_Tipos_basciso/tipos/any.ts
@@ -1,5 +1,18 @@
 (() => {
 
-    
+    let avenger:any = 123
+    const existe
+    let power
 
+    avenger = 'Dr strange'
+    //console.log(avenger.charAt(0));
+    console.log((avenger as string).charAt(0));
+    
+    avenger = 150.23256415
+    //console.log(avenger.toFixed(2));
+    console.log((<number>avenger).toFixed(2));
+    
+    console.log(existe);
+    console.log(power);
+    
 })()
