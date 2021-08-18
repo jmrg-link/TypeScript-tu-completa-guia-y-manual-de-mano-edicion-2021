88b4f151a2188f6adb9b6f9068f1c2f43b8230db
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-08-18 22:33:36 +0200

    Añadiendo ficheros args:function-types

---
+++ sc4_Funciones_y_objetos/bases/funciones/function-types.js
@@ -0,0 +1,19 @@
+"use strict";
+(function () {
+    var addNumber = function (a, b) { return a + b; };
+    var saveTheWorld = function () { return "El mundo esta salvado"; };
+    var greet = function (name) { return "hola " + name; };
+    // let myFunction:Function
+    // solo es precio definir el tipo en los parametros de la funcion
+    var myFunction;
+    //! extension better-comments
+    //* myFunction= 10
+    //* console.log(myFunction);
+    // la funcion addNumber tiene dos parametros a y b de tipo number para realizar en la funcion una suma.
+    // myFunction = addNumber
+    // console.log(myFunction(1,2));
+    myFunction = greet;
+    console.log(myFunction('Jesus'));
+    //? myFunction = saveTheWorld
+    //? console.log(myFunction());
+})();
