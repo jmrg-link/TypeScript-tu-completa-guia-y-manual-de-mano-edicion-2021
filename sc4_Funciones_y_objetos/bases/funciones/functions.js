a6d42b150643cf396a9f49398f130f58e660b59c
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-08-18 20:36:07 +0200

    Añadiendo fichero functions

--- sc4_Funciones_y_objetos/bases/funciones/functions.js
+++ sc4_Funciones_y_objetos/bases/funciones/functions.js
@@ -1,3 +1,13 @@
 "use strict";
 (function () {
+    var hero = 'Flash';
+    var returnName = function () {
+        return hero;
+    };
+    var activateBatiSeñal = function () {
+        return 'Batiseñal activated';
+    };
+    console.log(typeof activateBatiSeñal() + ' = ' + activateBatiSeñal());
+    var heroName = returnName();
+    console.log("" + heroName);
 })();
--- sc4_Funciones_y_objetos/bases/funciones/functions.ts
+++ sc4_Funciones_y_objetos/bases/funciones/functions.ts
@@ -1,3 +1,17 @@
 (()=>{
+    const hero: string = 'Flash'
+    
+    const returnName = ():string =>{
+        return hero
+    }
 
+    const activateBatiSeñal = ():string => {
+        return 'Batiseñal activated'
+    } 
+
+    console.log(typeof activateBatiSeñal() + ' = ' + activateBatiSeñal())
+
+    const heroName = returnName()
+    console.log(`${heroName}`);
+    
 })()
